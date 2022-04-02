f2fae6071307a53f69d79a805f6997998951f31f
aracelly <aracellybootcamp@gmail.com>
2022-04-02 04:08:05 -0300

    cambio en nombre de id y clases (mas descriptivos) y cambio de estilo

--- assets/script/calculadora.js
+++ assets/script/calculadora.js
@@ -1,44 +1,47 @@
 // Los eventos FrontEnd pueden ser manejados con JavaScript nativo o con Jquery.
 // Integre las operaciones básicas de una calculadora (suma, resta, multiplicación y división) y utilice gráficamente la librería Bootstrap para la visualización de la calculadora.
 
-
-const eq = document.getElementById('eq');
-const c= document.getElementById('c')
+// seleccionando elementos del DOM
+const equals = document.getElementById('equals');
+const clear = document.getElementById('clear')
 const numbers = document.querySelectorAll('.number');
 const operators = document.querySelectorAll('.operator');
-const dato = document.getElementById('dato');
+const data = document.getElementById('data');
+const error = document.getElementById('error')
 
+//formatear a numero
+let numFormat = num => new Intl.NumberFormat('cl').format(num);
 
+//Expresiones regulares
+const regExpOperation = /^(0|[1-9][0-9]*)([+-\/\*]{1}(0|[1-9][0-9]*))+$/;
 
+//Eventos
 numbers.forEach(el => {
   el.addEventListener('click', evt => {
     let num = parseInt(evt.currentTarget.dataset.id);
-    dato.innerHTML += num;
+    data.innerHTML += num;
   })
 })
 
 operators.forEach(el => {
   el.addEventListener('click', evt => {
     let op = evt.currentTarget.dataset.op;
-    dato.innerHTML += op;
+    data.innerHTML += op;
   })
 })
 
+equals.addEventListener('click', evt => {
+  if (data.innerHTML == "")
+    return;
 
-const re=/^([0-9]+[+-]{1}[0-9]+)+$/;
-
-eq.addEventListener('click', evt => {
-
-  console.log(dato.innerHTML)
-  if( re.test(dato.innerHTML)){
-    console.log("esta bien")
-  }  
-  else{
-    console.log('esta mal')
-  }
-  dato.innerHTML = eval(dato.innerHTML).toFixed(2);
+  if (regExpOperation.test(data.innerHTML)) {
+    error.classList.add("d-none");
+    data.innerHTML = eval(data.innerHTML).toFixed(2);
+  } else
+    error.classList.remove("d-none");
 })
 
-c.addEventListener('click', evt => {
-  dato.innerHTML = '';
+clear.addEventListener('click', evt => {
+  data.innerHTML = '';
+  error.classList.add("d-none");
 })
