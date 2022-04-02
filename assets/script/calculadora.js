7799b8deedd08f404dd13686035e889bfe647ea3
aracelly <aracellybootcamp@gmail.com>
2022-04-02 15:57:23 -0300

    se actualiza la operacion en base a el último ingreso

--- assets/script/calculadora.js
+++ assets/script/calculadora.js
@@ -1,44 +1,61 @@
 // Los eventos FrontEnd pueden ser manejados con JavaScript nativo o con Jquery.
 // Integre las operaciones básicas de una calculadora (suma, resta, multiplicación y división) y utilice gráficamente la librería Bootstrap para la visualización de la calculadora.
 
-// seleccionando elementos del DOM
-const equals = document.getElementById('equals');
-const clear = document.getElementById('clear')
+//seleccionando elementos del DOM
 const numbers = document.querySelectorAll('.number');
 const operators = document.querySelectorAll('.operator');
+const equals = document.getElementById('equals');
+const clear = document.getElementById('clear')
 const data = document.getElementById('data');
 const error = document.getElementById('error')
 
-//formatear a numero
+//Formatear a numero
 let numFormat = num => new Intl.NumberFormat('cl').format(num);
 
 //Expresiones regulares
-const regExpOperation = /^(0|[1-9][0-9]*)([+-\/\*]{1}(0|[1-9][0-9]*))+$/;
+const regExpNumber = /^[+-]?(0|[1-9][0-9]*)$/;
+const regExpOperation = /^[+-]?(0|[1-9][0-9]*)([+-\/\*]{1}(0|[1-9][0-9]*))+$/;
 
 //Eventos
 numbers.forEach(el => {
   el.addEventListener('click', evt => {
-    let num = parseInt(evt.currentTarget.dataset.id);
-    data.innerHTML += num;
+    let num = evt.currentTarget.dataset.num;
+    let splitPrevStr = data.innerHTML.split('/[+-\/\*]/');
+    let lastPrevElement = splitPrevStr[splitPrevStr.length - 1];
+    //puedo tener 0 pero no 00001, reviso el ultimo elemento el cero es reemplazable
+    if (lastPrevElement == '0')
+      data.innerHTML = data.innerHTML.slice(0, data.innerHTML.length - 1) + num
+    else
+      data.innerHTML += num;
   })
 })
 
 operators.forEach(el => {
   el.addEventListener('click', evt => {
     let op = evt.currentTarget.dataset.op;
-    data.innerHTML += op;
+    let previousStr = data.innerHTML;
+    let strLength = previousStr.length;
+    // Si el string es un numero o es una expresion algebraica 
+    if (regExpNumber.test(previousStr) || regExpOperation.test(previousStr))
+      data.innerHTML += op
+    else
+      //saco el ultimo operador en el string y lo uno al nuevo operador
+      data.innerHTML = previousStr.slice(0, strLength - 1) + op;
   })
 })
 
 equals.addEventListener('click', evt => {
+
   if (data.innerHTML == "")
     return;
-
-  if (regExpOperation.test(data.innerHTML)) {
+  //data.innerHTML viene sin el =, pero puede venir con un operador en el final por eso validamos.  
+  if (regExpNumber.test(data.innerHTML) || regExpOperation.test(data.innerHTML)) {
     error.classList.add("d-none");
-    data.innerHTML = eval(data.innerHTML).toFixed(2);
-  } else
+    data.innerHTML = numFormat(eval(data.innerHTML).toFixed(2));
+  } else {
+    error.innerHTML = "Error, ingreso de ejemplo: 5+2*3="
     error.classList.remove("d-none");
+  }
 })
 
 clear.addEventListener('click', evt => {
