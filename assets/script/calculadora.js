eb4207d944dd137518520466bcebea66191d8131
aracelly <aracellybootcamp@gmail.com>
2022-04-02 02:31:06 -0300

    avance calculadora

--- assets/script/calculadora.js
+++ assets/script/calculadora.js
@@ -1,2 +1,44 @@
 // Los eventos FrontEnd pueden ser manejados con JavaScript nativo o con Jquery.
 // Integre las operaciones básicas de una calculadora (suma, resta, multiplicación y división) y utilice gráficamente la librería Bootstrap para la visualización de la calculadora.
+
+
+const eq = document.getElementById('eq');
+const c= document.getElementById('c')
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
+const dato = document.getElementById('dato');
+
+
+
+numbers.forEach(el => {
+  el.addEventListener('click', evt => {
+    let num = parseInt(evt.currentTarget.dataset.id);
+    dato.innerHTML += num;
+  })
+})
+
+operators.forEach(el => {
+  el.addEventListener('click', evt => {
+    let op = evt.currentTarget.dataset.op;
+    dato.innerHTML += op;
+  })
+})
+
+
+const re=/^([0-9]+[+-]{1}[0-9]+)+$/;
+
+eq.addEventListener('click', evt => {
+
+  console.log(dato.innerHTML)
+  if( re.test(dato.innerHTML)){
+    console.log("esta bien")
+  }  
+  else{
+    console.log('esta mal')
+  }
+  dato.innerHTML = eval(dato.innerHTML).toFixed(2);
+})
+
+c.addEventListener('click', evt => {
+  dato.innerHTML = '';
+})
