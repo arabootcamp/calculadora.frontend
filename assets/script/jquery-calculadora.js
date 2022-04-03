608270ac4263dd51e9cf130661956fc8211a67bb
aracelly <aracellybootcamp@gmail.com>
2022-04-03 19:51:57 -0400

    se idento y realizo check html

--- assets/script/jquery-calculadora.js
+++ assets/script/jquery-calculadora.js
@@ -1,12 +1,12 @@
-$(function() {
+$(function () {
   // Handler for .ready() called.
 
   $('#jquery-title').text("Bienvenido a la calculadora realizada por el Grupo-03");
   $('#jquery-title').css({
-    "color":"#007bFF",
+    "color": "#007bFF",
     "font-family": "'Play', sans-serif",
-    "margin":"3rem 0",
-    "text-align":"center"
+    "margin": "3rem 0",
+    "text-align": "center"
   })
 
 });
